0e54df398fe0cfa6d4493ef2fa9c228d5e680240
EFREN NARVAEZ <efrennm86@gmail.com>
2023-10-14 14:43:38 -0600

    Obtenemos las respuestas mezcladas - Falta crear su funcionalidad

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const botonGenerar = document.querySelector('#botonGenerar');
 const dificultad = document.querySelector('#dificultad');
 const respuesta = document.querySelector('#respuesta');
 const categoria = document.querySelector('#categoria');
+const trivia= document.querySelector('#trivia');
 
 botonGenerar.addEventListener('click', e => {
     let api='https://opentdb.com/api.php?amount=10';
@@ -29,26 +30,35 @@ async function llamarApi(cadena){
     const resultado = await fetch(api);
     const resultado2 = await resultado.json();
     imprimirPreguntas(resultado2.results);
-    console.log(resultado2.results);
+ 
 }
 
 function imprimirPreguntas(arreglo){
     let preguntas = arreglo;
     let contador=0;
     const cadena=preguntas.map(pregunta => {
-        let respuestas=pregunta.correct_answer.concat(pregunta.incorrect_answers);
-        /* respuestas=respuestas.flat(); */
-        console.log(respuestas);
-        return 
+        //Almacenamos las respuestas
+        let respuestas=pregunta.incorrect_answers;
+        //Agregamos la respuesta correcta a las incorrectas
+        respuestas.push(pregunta.correct_answer);
+        //Revolvemos un array
+        respuestas.sort(function() { return Math.random() - 0.5 });
+        //Volvemos cadena de respuestas
+        respuestas=respuestas.join('     ');
+        
+        let cadena2=
         `<div class='preguntaContenedor' data='${contador}'>
             <div class='pregunta'>
                 ${pregunta.question}
             </div>
             <div class='respuestas'>
-                
+                ${respuestas}
             </div>    
         </div>`; 
-        
+        contador++;
+        return cadena2;
     });
+    console.log(cadena);
+    trivia.innerHTML=cadena;
 }
 

65999726f9c797774655f40a7d52cd8e954440a5
EFREN NARVAEZ <efrennm86@gmail.com>
2023-10-17 00:09:39 -0600

    La aplicación ya califica y evalua - Falta darle unos estilos

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const dificultad = document.querySelector('#dificultad');
 const respuesta = document.querySelector('#respuesta');
 const categoria = document.querySelector('#categoria');
 const trivia= document.querySelector('#trivia');
+let numeroPreguntas=0;
+let calificacion=0;
 
 botonGenerar.addEventListener('click', e => {
     let api='https://opentdb.com/api.php?amount=10';
@@ -29,8 +31,42 @@ async function llamarApi(cadena){
     let api=cadena;
     const resultado = await fetch(api);
     const resultado2 = await resultado.json();
+    calificacion=0;
+    numeroPreguntas=0;
+
+    //Generamos la trivia
     imprimirPreguntas(resultado2.results);
     //console.log(resultado2);
+
+    //Agregamos funcionalidad a las respuestas
+    let botones = document.querySelectorAll('.respuesta');
+    
+    botones.forEach(element => {
+        element.addEventListener('click', e =>{
+            element.style.fontSize='30px';
+            numeroPreguntas++;
+            if(element.getAttribute('data')==1){
+                calificacion++;
+                
+                bloquearPregunta(element.parentNode.parentNode, 'correcto');
+                colorearPregunta(element.parentNode.parentNode, 'correcto');
+                
+                               
+            }else{
+                bloquearPregunta(element.parentNode.parentNode, 'incorrecto');
+                colorearPregunta(element.parentNode.parentNode, 'incorrecto');
+
+            }
+        
+            
+            calificar();
+        });
+
+    });
+
+    //Checamos si ya terminó el juego y, de ser así, se imprime su calificación
+    
+
 }
 
 function imprimirPreguntas(arreglo){
@@ -70,8 +106,27 @@ function imprimirPreguntas(arreglo){
 
 function imprimirRespuestas(respuestas){
     const respuestas2 = respuestas.map(respuesta => {
-        return `<span class='respuesta' data='${respuesta[0]}'>${respuesta[1]}</span>`;
+        return `<button class='btn btn-success respuesta' data='${respuesta[0]}'>${respuesta[1]}</span>`;
     });
     return respuestas2;
 }
 
+function bloquearPregunta(elemento, cadena){
+    let hijos = elemento.childNodes[3].childNodes;   
+    hijos.forEach(element => {
+        element.disabled=true;
+    });
+}
+
+function colorearPregunta(elemento, cadena){
+    cadena == 'correcto'? elemento.style.backgroundColor="#D8EFC9" : elemento.style.backgroundColor="#F8C4B5"; 
+            
+}
+
+function calificar(){
+    console.log(calificacion + " " + numeroPreguntas);
+    console.log("-----");
+    if(numeroPreguntas==10){
+        alert("Su calificación es de "+calificacion+"/"+numeroPreguntas);
+    }
+}

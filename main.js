9727ab0010221f37e37a0bad6d336c9541cf0c28
EFREN NARVAEZ <efrennm86@gmail.com>
2023-10-16 22:07:55 -0600

    Falta capturar los acierto

--- main.js
+++ main.js
@@ -30,35 +30,48 @@ async function llamarApi(cadena){
     const resultado = await fetch(api);
     const resultado2 = await resultado.json();
     imprimirPreguntas(resultado2.results);
- 
+    //console.log(resultado2);
 }
 
 function imprimirPreguntas(arreglo){
     let preguntas = arreglo;
     let contador=0;
     const cadena=preguntas.map(pregunta => {
+        let respuestas=[];
         //Almacenamos las respuestas
-        let respuestas=pregunta.incorrect_answers;
+        //Vamos a ponerle a las respuestas incorrectas un identificador '0'
+         
+        for(let x=0; x<pregunta.incorrect_answers.length; x++){
+            respuestas.push([0,pregunta.incorrect_answers[x]]);
+                 
+        }
+        
         //Agregamos la respuesta correcta a las incorrectas
-        respuestas.push(pregunta.correct_answer);
+        respuestas.push([1, pregunta.correct_answer]);
         //Revolvemos un array
         respuestas.sort(function() { return Math.random() - 0.5 });
         //Volvemos cadena de respuestas
-        respuestas=respuestas.join('     ');
-        
+        respuestas=imprimirRespuestas(respuestas);
+        respuestas=respuestas.join(' ');
         let cadena2=
         `<div class='preguntaContenedor' data='${contador}'>
             <div class='pregunta'>
-                ${pregunta.question}
+                ${contador+1}.- ${pregunta.question}
             </div>
             <div class='respuestas'>
                 ${respuestas}
             </div>    
         </div>`; 
         contador++;
-        return cadena2;
+        return cadena2; 
     });
-    console.log(cadena);
     trivia.innerHTML=cadena;
+}
+
+function imprimirRespuestas(respuestas){
+    const respuestas2 = respuestas.map(respuesta => {
+        return `<span class='respuesta' data='${respuesta[0]}'>${respuesta[1]}</span>`;
+    });
+    return respuestas2;
 }
 
